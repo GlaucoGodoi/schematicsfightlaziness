6101265d8ca358a6ed06b686453f17540c14de08
glauco godoi <glauco.godoi@outlook.com>
2018-06-09 21:29:24 +0100

    Two rules implemented

--- src/p18/index.ts
+++ src/p18/index.ts
@@ -6,7 +6,7 @@ export interface PathShortCutsParameters {
   showNewContent: boolean;
 
   // If true a backup named tsconfig.json.Backup will be created.
-  createBackup:boolean;
+  createBackup: boolean;
 }
 
 export interface TsconfigFragment {
@@ -17,22 +17,38 @@ export interface TsconfigFragment {
 
 export class BasicPathConfiguration {
   public paths = new Array<{ shortcut: string, relativePath: string }>();
+  public itemsForSearchReplace: Array<{ shortcut: string, relativePath: string }>;
 
   constructor() {
     // Here you can add the folders and shortcuts that make sense to your environment
     this.paths.push({ shortcut: '@configs/*', relativePath: 'src/app/configs/*' });
-    this.paths.push({ shortcut: '@shared/*', relativePath: 'src/app/shared/*' });
+    this.paths.push({ shortcut: '@sharedServices*', relativePath: 'src/app/shared/services*' });
     this.paths.push({ shortcut: '@environments/*', relativePath: 'src/environments/*' });
-    this.paths.push({ shortcut: '@components/', relativePath: 'src/app/components/*' });
+    this.paths.push({ shortcut: '@sharedComponents/*', relativePath: 'src/app/shared/compo*' });
+
+    this.fillItemsForSearchReplace();
+  }
+
+  // This function will remove the noise from the configuration
+  private fillItemsForSearchReplace(): void {
+    this.itemsForSearchReplace = this.paths.map(item => {
+      return {
+        shortcut: item.shortcut.replace('*', ''),
+        relativePath: item.relativePath.replace('*', '')
+      };
+    });
   }
 }
 
-// This is the factory for the shortcut 
+// This is the factory (Rule) that will add the configuration to tsconfig.json 
 export function AddFolderShortcuts(options: PathShortCutsParameters): Rule {
   return (tree: Tree, _context: SchematicContext) => {
 
     //Search for tsconfig.json
-    const tsConfigFile = tree.root.subfiles.find((value: PathFragment) => { return value === 'tsconfig.json' });
+    const tsConfigFile = tree.root.subfiles.find((value: PathFragment) => {
+      return value === 'tsconfig.json'
+    });
+
     if (tsConfigFile) {
       _context.logger.info(`tsconfig.json found!`);
     } else {
@@ -43,7 +59,8 @@ export function AddFolderShortcuts(options: PathShortCutsParameters): Rule {
 
     //Read the file content
     const tsConfigContent = tree.read(tree.root.path + '/' + tsConfigFile);
-    const tsConfigData: TsconfigFragment = JSON.parse(tsConfigContent ? tsConfigContent.toString() : 'void');
+    const tsConfigData: TsconfigFragment =
+      JSON.parse(tsConfigContent ? tsConfigContent.toString() : 'void');
 
     //Check if the data already contain some path configuration
     if (tsConfigData.compilerOptions.hasOwnProperty('paths')) {
@@ -53,8 +70,8 @@ export function AddFolderShortcuts(options: PathShortCutsParameters): Rule {
     }
 
     //Create a backupfile
-    if(options.createBackup){
-      tree.create(`${tsConfigFile}.Backup`,JSON.stringify(tsConfigData,null, ' '));
+    if (options.createBackup) {
+      tree.create(`${tsConfigFile}.Backup`, JSON.stringify(tsConfigData, null, ' '));
     }
 
     //Add the new content to tsconfig.json
@@ -76,6 +93,47 @@ export function AddFolderShortcuts(options: PathShortCutsParameters): Rule {
     // Write the changes to the tree. Later the schematics infrastructure will handle the file update.
     tree.overwrite(tsConfigFile, JSON.stringify(tsConfigData, null, ' '));
 
+    return tree;
+  };
+}
+
+//////////////////////////////////// update imports /////////////////////////////
+
+export interface UpdateImportsParameters {
+  // This will be the initial folder to start the searching.
+  targetFolder: string
+}
+
+// This is the factory (Rule) that will update the .ts files 
+export function UpdateImportStatements(options: UpdateImportsParameters): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+
+    // Setup the initial folder to process
+    options.targetFolder =
+      options.targetFolder || tree.root.subdirs[tree.root.subdirs.length - 1];
+    
+      _context.logger
+      .info(`The process will scan files starting at the folder ${options.targetFolder}.`);
+
+    const shortcutConfig = new BasicPathConfiguration();
+
+    tree.getDir(options.targetFolder).visit(path => {
+      if (path.endsWith('.ts')) {
+        
+        const fileContent: Buffer | null = tree.read(path);
+        
+        if (fileContent) {
+          let textContent = fileContent.toString();
+
+          shortcutConfig.itemsForSearchReplace.forEach(item => {
+            if (textContent.indexOf(item.relativePath) > -1) {
+              textContent = textContent.replace(item.relativePath, item.shortcut);
+              tree.overwrite(path,textContent);
+            }
+          });
+        }
+      }
+    });
     return tree;
   };
 }
